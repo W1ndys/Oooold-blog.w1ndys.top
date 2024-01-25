2287c92a64991534d47aae7dec16b9dab57f81da
W1ndys <w1ndys@outlook.com>
2024-01-25 21:24:26 +0800

    deploy from hexo-admin

---
+++ 一键卸载加密插件.sh
@@ -0,0 +1,5 @@
+printf "\033[32mINFO \033[0m 正在卸载加密插件...\n"
+npm un --save hexo-blog-encrypt
+printf "\033[32mINFO \033[0m 安装完成，请在配置文件里配置相关内容...\n"
+printf "\033[32mINFO \033[0m 正在停顿10s可供查看情况！无异常可Ctrl+C退出\n"
+sleep 10s
