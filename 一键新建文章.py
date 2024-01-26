20a59b7a51414c656ab3a3df820da82252ec166c
W1ndys <w1ndys@outlook.com>
2024-01-26 17:03:55 +0800

    deploy from hexo-admin

--- 一键新建文章.py
+++ 一键新建文章.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python
 import os
 from datetime import datetime
+import re
+import subprocess
+
 
 def create_markdown_file(title, tags, categories):
-    # 获取脚本所在目录的绝对路径
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # 设置文件路径和名称
-    file_name = f"{title.replace(' ', '_').lower()}.md"
-    file_path = os.path.join(script_dir, "source/_posts", file_name)
+    try:
+        # 获取脚本所在目录的绝对路径
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # 检查目录是否存在，如果不存在则创建
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        # 设置文件路径和名称
+        POSTS_DIR = os.path.join(script_dir, "source/_posts")
+        # 使用正则表达式处理文件名
+        file_name = re.sub(r'[\\/:*?"<>|]', "-", title) + ".md"
+        file_path = os.path.join(POSTS_DIR, file_name)
 
-    # 获取当前日期
-    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # 检查目录是否存在，如果不存在则创建
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    # 将输入的空格隔开的标签和分类转换为英文逗号隔开的形式
-    tags = ','.join(map(str.strip, tags.split()))
-    categories = ','.join(map(str.strip, categories.split()))
+        # 获取当前日期
+        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # 构建front-matter
-    front_matter = f"---\n" \
-                   f"title: {title}\n" \
-                   f"tags: [{tags}]\n" \
-                   f"categories: [{categories}]\n" \
-                   f"date: {current_date}\n" \
-                   f"---\n\n"
+        # 将输入的空格隔开的标签和分类转换为英文逗号隔开的形式
+        tags = ",".join(map(str.strip, tags.split()))
+        categories = ",".join(map(str.strip, categories.split()))
 
-    # 创建Markdown文件并写入front-matter
-    with open(file_path, 'w') as file:
-        file.write(front_matter)
+        # 构建front-matter
+        front_matter = (
+            f"---\n"
+            f"title: {title}\n"
+            f"tags: [{tags}]\n"
+            f"categories: [{categories}]\n"
+            f"date: {current_date}\n"
+            f"---\n\n"
+        )
 
-    print(f"Markdown文件 '{file_name}' 已成功创建。")
+        # 创建Markdown文件并写入front-matter
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(front_matter)
+
+        print(f"Markdown文件 '{file_name}' 已成功创建.")
+
+        # 使用默认方式打开文件
+        subprocess.Popen([file_path], shell=True)
+
+    except Exception as e:
+        print(f"创建Markdown文件时发生错误: {e}")
+
 
 # 示例用法
 print("—————————————————————————————————————————————————————————— ")
@@ -48,3 +63,6 @@ categories = input("请输入分类（多个分类用空格隔开）: ")
 
 # 调用函数创建Markdown文件
 create_markdown_file(title, tags, categories)
+
+# # 按 Enter 键退出
+# input("按 Enter 键退出...")
